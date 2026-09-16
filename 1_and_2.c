6f7cbdf0eac680efbdbd502450eefbf89aede909
Agilesh N.V <159805437+Agilesh30@users.noreply.github.com>
2026-09-16 18:22:24 +0530

    Create 1_and_2.c

---
+++ 1_and_2.c
@@ -0,0 +1,47 @@
+/*Data Center Server Load Matrix
+A data center records the CPU usage of R servers across C time intervals. Store the values
+in a 2D array and calculate the total CPU usage of each server.
+Input Format
+ First line: R C
+ Next R lines: C integer CPU values
+Sample Input
+3 4
+40 50 60 70
+80 75 65 70
+30 45 50 55
+Sample Output
+Server 1: 220
+Server 2: 290
+Server 3: 180*/
+#include <stdio.h>
+int main()
+{
+    int R,C,val,sum=0,max,old_sum=0;
+    scanf("%d %d",&R,&C);
+
+    int cpu[R][C];
+    for (int i=0;i<R;i++)
+    {
+        printf("Enter consumption for server %d:",i+1);
+        for (int j=0;j<C;j++)
+        {
+            scanf("%d",&cpu[i][j]);
+        }
+
+    }
+      for (int i=0;i<R;i++)
+    {
+        for (int j=0;j<C;j++)
+        {
+            sum+=cpu[i][j];
+        }
+        printf("Server  %d:%d\n",i+1,sum);
+        if (old_sum<sum)
+        {
+        max=i;
+        old_sum=sum;
+        }
+        sum=0;
+    }
+    printf("The maximium consumption is in server %d",max+1);
+}
