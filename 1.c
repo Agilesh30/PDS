7dad196566b80f2a2e62e11d4becd34c958bbce5
Agilesh N.V <159805437+Agilesh30@users.noreply.github.com>
2026-09-22 23:02:37 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,40 +1,23 @@
-/*Data Center Server Load Matrix
-A data center records the CPU usage of R servers across C time intervals. Store the values
-in a 2D array and calculate the total CPU usage of each server.
-Input Format
- First line: R C
- Next R lines: C integer CPU values
+/*An IT company records the total hours a server was available and the total monitored hours.
+Calculate the server uptime percentage.
+Function Prototype
+float calculateUptime(int availableHours, int totalHours);
+Business Rule
+Uptime % = (Available Hours / Total Hours) � 100
 Sample Input
-3 4
-40 50 60 70
-80 75 65 70
-30 45 50 55
+95 100
 Sample Output
-Server 1: 220
-Server 2: 290
-Server 3: 180*/
+Uptime: 95.00%
+*/
+float calculateUptime(int availableHours, int totalHours)
+{
+    return (float)availableHours/totalHours*100;
+}
 #include <stdio.h>
 int main()
 {
-    int R,C,val,sum=0,max,old_sum=0;
-    scanf("%d %d",&R,&C);
-
-    int cpu[R][C];
-    for (int i=0;i<R;i++)
-    {
-        printf("Enter consumption for server %d:",i+1);
-        for (int j=0;j<C;j++)
-        {
-            scanf("%d",&cpu[i][j]);
-        }
-
-    }
-      for (int i=0;i<R;i++)
-    {
-        for (int j=0;j<C;j++)
-        {
-            sum+=cpu[i][j];
-        printf("Server  %d:%d\n",i+1,sum);
-   
-    }
+    int a,b;
+    printf("Enter Available hours and Total Hours:");
+    scanf("%d %d",&a,&b);
+    printf("Uptime :%0.2f",calculateUptime(a,b));
 }
