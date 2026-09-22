8c7c36533bcdb134e99ccf74fa382e441ee8664d
Agilesh N.V <159805437+Agilesh30@users.noreply.github.com>
2026-09-22 23:04:12 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
+void rotateRight(int arr[],int len,int rotate)
+{
+int i,j;
+int iteration=1,length=0;
+for (j=len-rotate+1;iteration<=rotate-1;j++)
+    {
+        printf("%d ",arr[j]);
+        iteration++;
+        length++;
+    }
+length++;
+for (j=0;length<=len;j++)
+    {
+        printf("%d ",arr[j]);
+        length++;
+    }
+}
+
 int main()
 {
-    int R,C,val,sum=0,max,old_sum=0;
-    scanf("%d %d",&R,&C);
-
-    int cpu[R][C];
-    for (int i=0;i<R;i++)
+    int n,k;
+    printf("Enter size of the array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("\nEnter values");
+    for (int i=0;i<n;i++)
     {
-        printf("Enter Product quantities for warehouse %d:",i+1);
-        for (int j=0;j<C;j++)
-        {
-            scanf("%d",&cpu[i][j]);
-        }
+        scanf("%d",&arr[i]);
+    }
+    printf("\nEnter Rotate value: ");
+    scanf("%d",&k);
+    rotateRight(arr,n,k);
 
-    }
-      for (int i=0;i<C;i++)
-    {
-        for (int j=0;j<R;j++)
-        {
-            sum+=cpu[j][i];
-    }
-            printf("Product  %d:%d\n",i+1,sum);
-        sum=0;
-    }}
+}
+/*
+10 20 30 40 50 60
+
+
+60 10 20 30 40 50   -- 5 0 1 2 3 4 --2 -->len-rotate+1 * 1 iteration (rotate-1)times -->
+50 60 10 20 30 40   -- 4 5 0 1 2 3 --3 -->6-3+1 --4 * 2 iteration 
+40 50 60 10 20 30   -- 3 4 5 0 1 2 --4
+*/
