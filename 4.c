23831bc7bd747edba800c2fc0b25407cfea9a601
Agilesh N.V <159805437+Agilesh30@users.noreply.github.com>
2026-09-22 23:03:21 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
+float calculateBonus(float a,int b)
+{
+    float bonus;
+    if (b<=60)
+        bonus=5;
+    else if (b<75)
+        bonus=10;
+    else if (b<90)
+        bonus=15;
+     else if (b<=100)
+        bonus=20;
+    bonus*=a/100;
+    return bonus;
+}
 int main()
 {
-    int row,col;
-    printf("Enter Team and total number of position");
-    scanf("%d %d",&row,&col);
-    int arr[row][col];
-    for (int i=0;i<row;i++)
-        {
-            printf ("Enter value for Team %d:",i+1);
-            for (int j=0;j<col;j++)
-                {
-                    scanf("%d",&arr[i][j]);
-                }
-        }
-    int find,flag=0;
-    printf("Enter the id to find: ");
-    scanf("%d",&find);
-    for (int i=0;i<row;i++)
-        {
-            for (int j=0;j<col;j++)
-                {
-                    if(arr[i][j]==find){
-                        flag=1;
-                        printf("Team :%d\nPosition :%d",i+1,j+1);
-                    break;
-                }
-        }  
-            if (flag==0)
-            {
-                printf("Id : %d is not found in the Team list",find);
-            }
-        }
+    float sal;
+    int score;
+    printf("Enter Salary and Score");
+    scanf("%f %d",&sal,&score);
+    printf("Bonus :%0.2f",calculateBonus(sal,score));
+
 }
